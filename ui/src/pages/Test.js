589c1abf99166ff87e82a35ef76c83b666cae488
Mateusz Piórowski <mateuszpiorowski@gmail.com>
2019-10-01 14:39:53 +0200

    main app file

---
+++ ui/src/pages/Test.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Test extends Component {
+
+
+  render({ data }) {
+    return (
+        <div>
+          <h1>TESTss</h1>
+          <h2>{data}</h2>
+        </div>
+    );
+  }
+}
+
+export default Test;
