63c4e0492e0813b18da03be35e46b947472a0dbe
Mateusz Piórowski <mateuszpiorowski@gmail.com>
2019-11-28 10:50:26 +0100

    docker config

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 mkdir -p ./files ./logs
 sudo chmod -R 777 ./files ./logs
+
+chmod +x init-letsencrypt.sh
+sudo sh init-letsencrypt.sh
+
 docker-compose -f ./docker/docker-compose.$1.yml up -d --build
