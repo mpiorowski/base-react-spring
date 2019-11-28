c7d99f841dffef04f898a9f685c890162cf73a7d
Mateusz Piórowski <mateuszpiorowski@gmail.com>
2019-11-28 11:21:35 +0100

    clean

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ mkdir -p ./files
 sudo chmod -R 777 ./files
 
 chmod +x ./docker/init-letsencrypt.sh
-sudo sh ./docker/init-letsencrypt.sh
+sudo sudo ./docker/init-letsencrypt.sh
 
 docker-compose -f ./docker/docker-compose.prod.yml up -d --build
